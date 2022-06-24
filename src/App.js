964dd1915d0d46065ce9e7ecc70765ba72554c3f
adhit-kun <adhityakunc@gmail.com>
2022-06-24 14:10:34 +0700

    React Experiment 101

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTrashCan} from '@fortawesome/free-solid-svg-icons';
+import {faCircleCheck} from '@fortawesome/free-solid-svg-icons';
+import {faPen} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
+
+
 function App() {
+ 
+  const [todo, setodo] = useState([
+      {'id': 1, 'title': "task1", 'status': true},
+      {'id': 2, 'title': "task2", 'status': false}
+    ]);
+
+  const [nTask, sTask] = useState('');
+  const [uTask, suTask] = useState('');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <h2> Todo </h2>
+      <br /><br />
+    
+
+      {todo && todo.length ? '' : 'No Task...' }
+      
+      {todo && todo
+      .sort((max, min) => max.id > min.id ? 1 :-1)
+      .map((list, index) => {
+        return(
+          <React.Fragment key={list.id}>
+            <div className='listBG center'>
+              
+              
+              <div className={list.status ? 'done': ''}>
+                <span className="listNumber"> {index + 1} </span>
+                <span className="listText"> {list.title} </span>
+              </div>
+
+              <div className='iconW'>
+                <span title='Complete/Ongoing'> 
+                  <FontAwesomeIcon icon={faCircleCheck}></FontAwesomeIcon>
+                </span>
+                <span title='Change list'> 
+                  <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
+                </span>
+                <span title='Delete list'> 
+                  <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+                </span>
+
+              </div>
+
+            </div>
+          </React.Fragment>
+
+        );
+      })}
+
     </div>
   );
+  
 }
 
 export default App;
